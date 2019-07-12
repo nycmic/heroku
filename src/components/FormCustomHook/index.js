142a9efb4ab7191c4e0c0f6939591cff30955365
Michael Kuzmovich <micnyc8@gmail.com>
2019-07-12 19:46:52 +0300

    Fix

--- src/components/FormCustomHook/index.js
+++ src/components/FormCustomHook/index.js
@@ -13,22 +13,6 @@ function encode(data) {
         .join("&");
 }
 
-function xwwwfurlenc(srcjson) {
-    if (typeof srcjson !== "object")
-        if (typeof console !== "undefined") {
-            console.log("\"srcjson\" is not a JSON object");
-            return null;
-        }
-    let u = encodeURIComponent;
-    var urljson = "";
-    var keys = Object.keys(srcjson);
-    for (var i = 0; i < keys.length; i++) {
-        urljson += u(keys[i]) + "=" + u(srcjson[keys[i]]);
-        if (i < (keys.length - 1)) urljson += "&";
-    }
-    return urljson;
-}
-
 export default ({ children }) => {
     const data = useStaticQuery(
         graphql`
@@ -72,27 +56,9 @@ export default ({ children }) => {
 
         form.setState[name](value);
     };
-
-    // const handleRecaptcha = value => {
-    //     console.log(value);
-    //     const formData = form.elms;
-    //     console.log(formData);
-    //     formData({ "g-recaptcha-response": value });
-    //     console.log(value);
-    //   };
-
     const handleSubmit = e => {
         e.preventDefault();
-        // var $form = $(this);
-        // $.post($form.attr("action"), $form.serialize()).then(function() {
-        //     alert("Thank you!");
         const formData = form.elms;
-        // });
-        const data1 = encode(formData);
-        console.log(encode({
-            "form-name": "contact",
-            ...formData
-        }));
         fetch("contact", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -105,46 +71,6 @@ export default ({ children }) => {
             .then(() => alert('Your message submit successfully'))
             .catch(error => alert(error));
     };
-
-    // const handleSubmit = event => {
-    //
-    //
-    //   event.preventDefault();
-    //
-    //   const formData = form.elms;
-    //   formData.webform_id = "contact";
-    //
-    //   formData.grant_type = "password";
-    //   formData.client_id = "2e2d73d0-520d-43b7-9363-a22ffc37e2f8";
-    //   formData.client_secret = "gatsby";
-    //   formData.username = "funnel";
-    //   formData.password = "admin";
-    //
-    //   console.log(formData);
-    //
-    //   // fetch('https://decoupled.devstages.com/oauth/token', {
-    //   //     method: 'POST',
-    //   //     headers: {
-    //   //         'Accept': 'application/json',
-    //   //         'Content-Type': 'multipart/form-data'
-    //   //     },
-    //   //     body: JSON.stringify(formData)
-    //   // })
-    //   //
-    //   // fetch('https://decoupled.devstages.com/webform_rest/submit?_format=json', {
-    //   //   method: 'POST',
-    //   //   headers: {
-    //   //     "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjA4OGU2OGFlZjFhN2E3MTBkMmQ2ZTIxYjQxN2U0MWIwOTBhNDQ1ZjEyMjhkYzk2MTljYWZlZmYwNDZiMzVjMmU1YmJiODRhNDczNjAwNDNmIn0.eyJhdWQiOiIyZTJkNzNkMC01MjBkLTQzYjctOTM2My1hMjJmZmMzN2UyZjgiLCJqdGkiOiIwODhlNjhhZWYxYTdhNzEwZDJkNmUyMWI0MTdlNDFiMDkwYTQ0NWYxMjI4ZGM5NjE5Y2FmZWZmMDQ2YjM1YzJlNWJiYjg0YTQ3MzYwMDQzZiIsImlhdCI6MTU2MjgzOTg3MywibmJmIjoxNTYyODM5ODczLCJleHAiOjE1NjI4ODQ1MTMsInN1YiI6IjEiLCJzY29wZXMiOlsiYXV0aGVudGljYXRlZCJdfQ.PCrbBc5J0Ks-S5Rl_sfSkNgLZT1INOdsqzcXoXpO6hsmrX51P1hP9JcORziwU3SVcQS2T-GGYShWb-9nzrfLntjJhnGysQxHcTITf87CxNQEAbqZDRGyqs7c3QklnkFctuLbdWRW5rkgD30AGwJE7ZfA0CSGPCTnXOjOk38accMFgPwCtdKI-NoYFA0tZ7Nwd_B4CpHWYpwclr6MDmPHd6epoiSmNodlelyF1LyDLTzMIVQsjUGdhK3mKGqcvjHZNUEbS_NHFMwjprtlCe_Oto_HGwaHGmOvtpJiHtkJgin8iS1SGXgx00SJ1omFuYy177-r2AVETT2V860r_hz5nZ6WrUm5KptG0jZRkSvnSLY17HYhiB8Z3cbzOfkso46i4gNkRh0J47byimayfdRO9GbGP7tloeBzcfJMIG7E9kpn7OqYPN7bzIO4lQW633IhbfAn4KU8HaImuqPLdNWez1oLsOfcZ0VNkgkzPHSj33aJv13T-m9y0IKyYNK-iuEJnRvljtbBXJHDlzAQHLeY6vQx4uIJHcyuL8e4NPHK36vtVDjVYwNLEknG2NL9Xyvi08otRFS-XOHTndoOhnQ9yztd8QMd50j5dNikNwiV2Z5grYBPekAOvgN7O7ufhGhZQp5YHZX6Zxax2arKTTis-f7t11dlwaxWoCb1LaWJmPM",
-    //   //     'Accept': 'application/json',
-    //   //     'Content-Type': 'application/json'
-    //   //   },
-    //   //   body: JSON.stringify(formData)
-    //   // })
-    // };
-
-
-
-
     return (
         <>
 
@@ -184,7 +110,7 @@ export default ({ children }) => {
                             <label>
                                 <input
                                     {... {'required': item.required}}
-                                    type="phone"
+                                    type="tel"
                                     name={item.name}
                                     placeholder = {item.title}
                                     value={form.elms[item.name]}
@@ -202,7 +128,7 @@ export default ({ children }) => {
 
                                 <input
                                     {... {'required': item.required}}
-                                    type="mail"
+                                    type="email"
                                     name={item.name}
                                     placeholder = {item.title}
                                     value={form.elms[item.name]}
@@ -254,102 +180,3 @@ export default ({ children }) => {
 
     )
 }
-
-// import React from "react"
-// import { StaticQuery, graphql } from "gatsby"
-// import Recaptcha from "react-google-recaptcha";
-//
-// function encode(data) {
-//     return Object.keys(data)
-//         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//         .join("&");
-// }
-//
-// export default class FormCustom extends React.Component {
-//     state = {
-//         firstName: "",
-//         lastName: "",
-//     };
-//
-//     handleInputChange = event => {
-//         const target = event.target;
-//         const value = target.value;
-//         const name = target.name;
-//         console.log(name);
-//         this.setState({
-//             [name]: value,
-//         })
-//     };
-//
-//     handleRecaptcha = value => {
-//         this.setState({ ["g-recaptcha-response"]: value });
-//     };
-//
-//     handleSubmit = event => {
-//         event.preventDefault();
-//         fetch('/', {
-//             method: 'POST',
-//             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//             body: encode({
-//                 "form-name": "contact",
-//                 "firstName": "test",
-//                 "lastName": "test@est.test",
-//             })
-//         })
-//                     .then(() => alert('Your message submit successfully'))
-//             .catch(error => alert(error));
-//     };
-//
-//     render() {
-//
-//         return (
-//
-//             <StaticQuery
-//                 query={graphql`
-//             query {
-//               form: webformWebform {
-//                 elements
-//                 title
-//               }
-//             }
-//           `}
-//                 render={data => (
-//                     <>
-//                         {/*{*/}
-//                         {/*console.log( getForm(data.form.elements))*/}
-//                         {/*}*/}
-//
-//                         <form name="contact" method="post"  data-netlify="true" onSubmit={this.handleSubmit}>
-//                             <input type="hidden" name="form-name" value="contact" />
-//                             {/*<Recaptcha*/}
-//                                 {/*sitekey="6LceP60UAAAAAAJraGxoXitOcUeJxQN0enAWiCXJ"*/}
-//                                 {/*onChange={this.handleRecaptcha}*/}
-//                             {/*/>*/}
-//                             <label>
-//                                 First name
-//                                 <input
-//                                     type="text"
-//                                     name="firstName"
-//                                     value={this.state.firstName}
-//                                     onChange={this.handleInputChange}
-//                                 />
-//                             </label>
-//                             <label>
-//                                 Last name
-//                                 <input
-//                                     type="text"
-//                                     name="lastName"
-//                                     value={this.state.lastName}
-//                                     onChange={this.handleInputChange}
-//                                 />
-//                             </label>
-//                             <button type="submit" className="form-submit">Submit</button>
-//                         </form>
-//
-//                     </>
-//                 )}
-//             />
-//
-//         )
-//     }
-// }
